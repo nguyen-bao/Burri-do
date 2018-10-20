476d60c64379561555cbb0ca230a93a5dbb8c92b
nguyen-bao <baozer600@gmail.com>
2018-10-19 23:03:13 -0400

    add helper classes

---
+++ app/src/main/java/com/baoandjon/burri_do/Sprite.java
@@ -0,0 +1,17 @@
+package com.baoandjon.burri_do;
+
+public class Sprite {
+    private String path;
+
+    public Sprite(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
